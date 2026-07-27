0877b904bc09f5e84199e2ba0ef3123dac03de14
vennro <vennro@gmail.com>
2026-07-27 16:29:06 +0700

    save

---
+++ mariadb/phpmyadmin/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+$i = 0;
+
+$i++;
+$cfg['Servers'][$i]['verbose'] = 'Mariadb 10.6';
+$cfg['Servers'][$i]['host'] = 'mariadb-container';
+$cfg['Servers'][$i]['port'] = '3306';
+$cfg['Servers'][$i]['auth_type'] = 'cookie';  # Ввод логина/пароля
+//$cfg['Servers'][$i]['auth_type'] = 'config';  # Автологин
+//$cfg['Servers'][$i]['user'] = 'root';
+//$cfg['Servers'][$i]['password'] = 'root';
